f0ba363b374114976b0c28f458360ff6969408e9
rhnathar <114830697+rhnathar@users.noreply.github.com>
2024-10-28 14:46:06 +0700

    Add files via upload

---
+++ Modul9/Akun.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul9
+{
+    public class Akun
+    {
+        private const int maxSaldo = 2000000;
+        protected int _saldo;
+        protected string password;
+
+        public Akun (int saldo, string password)
+        {
+            _saldo = saldo;
+            this.password = password;
+        }
+        public virtual void Setor(int jumlah)
+        {
+            Saldo += jumlah;
+        }
+
+        public virtual void Tarik(int jumlah)
+        {
+            Saldo -= jumlah;
+        }
+
+        public int Saldo
+        {
+            get
+            {
+                return _saldo;
+            }
+            set
+            {
+                if (value < 0)
+                    throw new InvalidOperationException("Saldo tidak bisa kurang dari 0 rupiah");
+                else if (value > maxSaldo)
+                    throw new InvalidOperationException("Saldo tidak boleh melebihi 2000000 rupiah");
+                else
+                    _saldo = value;
+            }
+        }
+        public bool PasswordTervalidasi (string password)
+        {
+            if (this.password == password)
+                return true;
+            else
+                return false;
+        }
+    }
+}
